c400d1d0e6dd81e75d7e4d14f6bc91b8b1a257cc
ali1x3 <avilaalex371@gmail.com>
2025-09-23 21:11:25 +0800

    added dodge mechanic to skeleton

--- src/player/character_body_2d.gd
+++ src/player/character_body_2d.gd
@@ -5,12 +5,17 @@ const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 const COYOTE_TIME = 0.10 # grace period that you can perform a normal jump after a ledge
 const BUFFER_TIME = 0.10
-const FRICTION = 100.0
 
 # booleans
-var DOUBLE_JUMPED = false
+var double_jumped = false
 var is_attacking = false
+var is_dodging = false
+
+
 var attack_string = 0
+var current_speed = SPEED
+var dodge_multiplier = 3.0
+
 
 # timers
 var coyote_timer = COYOTE_TIME
@@ -18,84 +23,140 @@ var buffer_timer = BUFFER_TIME
 
 # gravity
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var gravity_vector = Vector2(0.0, 980.0)
+var gravity_vector = Vector2(0.0, gravity)
 @onready var animations: AnimatedSprite2D = $animations
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		coyote_timer -= delta
-		if velocity.y > 0:
-			velocity += gravity_vector * delta
-		else :
-			velocity += gravity_vector * 1.3 * delta
-	else:
-		coyote_timer = COYOTE_TIME
-		DOUBLE_JUMPED = false
+# player state
+enum PlayerState { ATTACK, DODGE, GROUND_MOVE, AIR, IDLE }
+var state = PlayerState.IDLE
 
-	# Handle jump.
-	var taking_action = is_attacking
-	
-	if not taking_action:
-		run_listener()
-		attack_listener(delta)
-		jump_listener()
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+
+func _ready() -> void:
+	animations.animation_finished.connect(animation_finished)
+
+
+func _physics_process(delta: float) -> void:
+	handle_timers_helpers(delta)
+	handle_inputs()
+	animation_update()
+	apply_physics(delta)
 	move_and_slide()
 
+
+func animation_finished():
+	var animation = animations.animation
+	if is_attacking:
+		is_attacking = false
+	if animation == "dodge":
+		reset_dodge()
+
+func handle_timers_helpers(delta: float):
+	if is_on_floor():
+		coyote_timer = COYOTE_TIME
+		double_jumped = false
+	else:
+		coyote_timer -= delta
+
+func handle_inputs():
+	jump_listener()
+	run_listener()
+	if not is_attacking:
+		attack_listener()
+	dodge_listener()
+
+func handle_state():
+	pass
+	
+
+func handle_state_effects():
+	pass
+
+
+func apply_physics(delta: float):
+	if not is_on_floor():
+		if velocity.y > 0:
+			velocity += gravity_vector * delta
+		else:
+			velocity += gravity_vector * 1.3 * delta
+
+
+func animation_update() -> void:
+	match state:
+		PlayerState.DODGE:
+			print("dodge")
+			is_dodging = true
+			animations.play("dodge")
+			return
+		PlayerState.ATTACK:
+			print("attack")
+			attack_listener()
+			return
+			
+	if is_attacking or is_dodging:
+		return
+	match state:
+		PlayerState.IDLE:
+			animations.play("idle")
+		PlayerState.GROUND_MOVE:
+			animations.play("run")
+			print("run")
+		PlayerState.AIR:
+			# falling animation here
+			pass
+			
+			
+func dodge_listener() -> void:
+	if Input.is_action_just_pressed("dodge"):
+		current_speed *= dodge_multiplier
+		state = PlayerState.DODGE
+	
 func jump_listener() -> void:
 	if Input.is_action_just_pressed("jump"):
 		if is_on_floor() or coyote_timer > 0.0:
 			velocity.y = JUMP_VELOCITY
-		elif not DOUBLE_JUMPED:
+		elif not double_jumped:
 			velocity.y = JUMP_VELOCITY
-			DOUBLE_JUMPED = true
+			double_jumped = true
 	if Input.is_action_just_released("jump"):
 		velocity.y *= 0.3
+
 
 func run_listener() -> void:
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * current_speed
 		if direction < 0:
 			animations.flip_h = true
 		else:
 			animations.flip_h = false
-		animations.play("run")
+		if not is_attacking:
+			state = PlayerState.GROUND_MOVE
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animations.play("idle")
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		if not is_attacking or is_dodging:
+			state = PlayerState.IDLE
 
-func attack_listener(delta: float) -> void:
+
+func attack_listener() -> void:
 	if Input.is_action_just_pressed("attack"):
 		is_attacking = true
-		gravity_vector *= 0.03
-		velocity.y = 0
-		velocity.x *= 0.05
-		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-		
+		attack_string = (attack_string + 1) % 3
+		reset_dodge()
 		attack_string_handler()
-		animations.animation_finished.connect(attack_finished)
 
 
 func attack_string_handler() -> void:
 	if attack_string == 0:
 		animations.play("attack_string1")
 		print("attack string 1")
-		attack_string += 1
 	elif attack_string == 1:
 		animations.play("attack_string2")
 		print("attack string 2")
-		attack_string += 1
 	elif attack_string == 2:
 		animations.play("attack_string3")
 		print("attack string 3")
-		attack_string = 0
 
-
-
-func attack_finished() -> void:
-	is_attacking = false
-	gravity_vector = get_gravity()
-	
+func reset_dodge():
+	is_dodging = false
+	current_speed = SPEED
+	state = PlayerState.IDLE
